a593ae19ec9e0360a8295bae0fbb1c03c1df626e
Grzegorz Grabowski <grzegorz.grabowski@intel.com>
2017-04-01 15:12:42 -0500

    Output directory param added, dir duplicates handler implemented

--- rename_music.sh
+++ rename_music.sh
@@ -3,6 +3,8 @@
 # TO DO check if all tools are installed
 
 BASE_DIR=`pwd`
+OUT_DIR="$BASE_DIR"
+DIRO=0
 LOG_FILE="my_music.log"
 SORT_ARTIST=0
 SORT_GENRE=0
@@ -67,9 +69,10 @@ function show_help
     echo "   -q   quiet mode"
     echo "   -f   rename folder to format %artist% - %album%"
     echo "   -s   rename file/song to format %number% - %title% - %artist% - %album%"
+    echo "   -o   move renamed files/dirs to target_directory"
 }
 
-while getopts "hdvt?qi:fs" opt; do
+while getopts "hdvt?qi:fso:" opt; do
     case "$opt" in
       h|\?)
         show_help
@@ -78,11 +81,21 @@ while getopts "hdvt?qi:fs" opt; do
       v) VERBOSE=1 ;;
       t) TEST_RUN=1 ;;
       i) BASE_DIR=$OPTARG ;;
+      o) DIRO=1;
+         DIR_OUT=$OPTARG ;;
       q) QUIET=1;;
       f) FOLDER=1;;
       s) FILE=1;;
     esac
 done
+
+
+if [ ! -d "$DIR_OUT" ] ; then
+	log_i "Creating directory $DIR_OUT"
+	if [ "$TEST_RUN" != 1 ]; then
+	  mkdir -p "$DIR_OUT"
+	fi
+fi
 
 
 START=`date +%s`
@@ -96,8 +109,10 @@ while read -r dir; do
 	artist=""
 	album=""
 	
+	MOVE_IF_ALL_TAGS=1
 	while read -r file; do
 	
+	  
 	  FILE_NUM=$(($FILE_NUM+1))	
     base=`basename "$dir"` 
 
@@ -106,7 +121,10 @@ while read -r dir; do
 		title=`exiftool -s3 -title "$file"`
 		artist=`exiftool -s3 -artist "$file"`
 		album=`exiftool -s3 -album "$file" `  
-			
+		
+		if [[ -z "$title" ]] || [[ -z "$artist" ]] || [[ -z "$album" ]] || [[ -z "$num" ]]; then
+			MOVE_IF_ALL_TAGS=0
+		fi
 		ext=${file##*.}
 
     if [ $FILE_NUM -gt 1 ]; then
@@ -132,17 +150,31 @@ while read -r dir; do
 	done < <(find "$dir" -iname "*mp3" -maxdepth 1 -type f)
 	
 	
-  log_d "$dir : SAME_ARTIST = $SAME_ARTIST, FILE_NUM=$FILE_NUM"
+  log_d "$dir : SAME_ARTIST = $SAME_ARTIST, FILE_NUM=$FILE_NUM, ALL_TAG:$MOVE_IF_ALL_TAGS"
   if [ "$FOLDER" == 1 ] && [ "$FILE_NUM" -gt 0 ] && [ "$SAME_ARTIST" -eq 1 ] && [ "$SAME_ALBUM" -eq 1 ]; then
 
     if [ "$artist" != "" ] && [ "$album" != "" ]; then
-      new_dir="${dir%/*}/$artist - $album"
+      if [ "$DIRO" == 1 ]; then
+         new_dir="$DIR_OUT/$artist - $album"
+      else
+        new_dir="${dir%/*}/$artist - $album"
+      fi
+ 
+      # if exists increase version or overwrite
+      if [ -d "$new_dir" ] ; then
+        date=`date`
+	      new_dir="$new_dir ($date)"
+	      if [ "$TEST_RUN" != 1 ]; then
+	        mkdir -p "$new_dir"
+	      fi
+      fi     
+        
 	    log_i "Renaming dir $dir -> $new_dir"
 	    if [ "$TEST_RUN" != 1 ]; then
-		    mv "$dir" "$artist - $album"
+		    mv "$dir" "$new_dir"
 		   fi
 		else
-		  log_i "Missing artist or album tag"
+		  log_i "Missing artist or album tag in dir : $dir"
 		fi
 	fi
 done < <(find "$BASE_DIR" -depth -type d)
